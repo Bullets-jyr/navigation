a6da6b2632ede6fdd174ccd59e9e36250cb11dfd
Bullets-jyr <jyr@bullets.co.kr>
2023-04-22 21:16:42 +0900

    알고있으면 좋은 Push 메소드들

--- lib/screen/route_two_screen.dart
+++ lib/screen/route_two_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/layout/main_layout.dart';
+import 'package:navigation/screen/route_three_screen.dart';
 
 class RouteTwoScreen extends StatelessWidget {
   const RouteTwoScreen({Key? key}) : super(key: key);
@@ -34,6 +35,51 @@ class RouteTwoScreen extends StatelessWidget {
             );
           },
           child: Text('Push Named'),
+        ),
+        // [HomeScreen(), RouteOne(), RouteTwo(), RouteThreeScreen()]
+        // 결과 : [HomeScreen(), RouteOne(), RouteThreeScreen()]
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(
+                builder: (_) => RouteThreeScreen(),
+              ),
+            );
+          },
+          child: Text('Push Replacement'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushReplacementNamed(
+              '/three',
+            );
+          },
+          child: Text('Push Replacement Named'),
+        ),
+        // [HomeScreen(), RouteOne(), RouteTwo(), RouteThreeScreen()]
+        // 결과 : [HomeScreen()]
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(
+                builder: (_) => RouteThreeScreen(),
+              ),
+              // 우리가 Named Route로 Push를 하지 않은 경우, name이 null일 수도 있습니다.
+              // Named Route로 Push를 했다면, 특정 Route까지만 삭제를 할 수 있습니다.
+              // false : 해당 route를 스택에서 제거
+              (route) => route.settings.name == '/',
+            );
+          },
+          child: Text('Push And Remove Until'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushNamedAndRemoveUntil(
+              '/three',
+              (route) => route.settings.name == '/',
+            );
+          },
+          child: Text('Push Named And Remove Until'),
         ),
       ],
     );
